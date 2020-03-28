6d79a12a0b985a71e249717fba640a6a6ae439f5
brunosilva <brunosilva@unimestre.com>
2020-03-28 19:47:02 -0300

    refatoração

--- modulo-01(interagir com a DOM)/desafio-01/app.js
+++ modulo-01(interagir com a DOM)/desafio-01/app.js
@@ -6,9 +6,6 @@ new Vue({
         LINK: 'https://i.pinimg.com/236x/30/9d/23/309d23bec8975c7f283891906204d65d.jpg'
     },
     methods: {
-        idade3(){
-            return this.IDADE * 3;
-        },
         random(){
             return Math.random();
         }
